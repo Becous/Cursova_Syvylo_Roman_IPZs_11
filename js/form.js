814390806c5d29014a3ee131623ccea45dafa392
Becous <becousgame@gmail.com>
2023-12-19 23:06:17 +0200

    добавив модалку

--- js/form.js
+++ js/form.js
@@ -27,3 +27,30 @@ function sendMessage() {
       // Обробка інших помилок
     });
 }
+function sendMessageModal() {
+  const nameInput = document.querySelector('input[name="name_modal"]').value;
+  const emailInput = document.querySelector('input[name="email_modal"]').value;
+  const messageInput = document.querySelector('textarea[name="message_modal"]').value;
+  // Ідентифікатор групи або користувача, куди ви хочете надіслати повідомлення
+
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  const message = `Name: ${nameInput}, Email: ${emailInput}%0AПовідомлення: ${messageInput}`; // Формування повідомлення з даними
+
+  fetch(`${url}?chat_id=${chatId}&text=${message}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Повідомлення відправлено до Telegram!");
+        // Додайте код для обробки успішної відправки повідомлення
+      } else {
+        console.error("Не вдалося відправити повідомлення до Telegram");
+        // Додайте код для обробки помилки відправки повідомлення
+      }
+    })
+    .catch((error) => {
+      console.error("Помилка відправлення повідомлення до Telegram:", error);
+      // Обробка інших помилок
+    });
+}
