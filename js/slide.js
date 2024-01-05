e986fcd0c8c2a0a263144a3c11d6e1bbf35bbad4
Becous <becousgame@gmail.com>
2024-01-05 20:17:58 +0200

    плавний скрол

--- js/slide.js
+++ js/slide.js
@@ -4,3 +4,23 @@ function scrollToElement(elementId) {
     element.scrollIntoView({ behavior: "smooth" });
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("nav ul li a");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        const offsetTop = targetSection.offsetTop;
+        window.scrollTo({
+          top: offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
